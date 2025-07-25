c9193fafbda1e26ca5eb08db2329cd61bb246f62
zahmed714 <otez.light@gmail.com>
2025-07-25 17:16:37 -0300

    Fix

--- client/src/lib/constants/characters.ts
+++ client/src/lib/constants/characters.ts
@@ -7,7 +7,7 @@ export type Character = {
 export const CHARACTERS: Character[] = [
   { id: "aiku", name: "Oliver Aiku", avatar: "/img/characters/Oliver_Aiku.png" },
   { id: "gaga", name: "Gin Gagamaru", avatar: "/img/characters/Gin_Gagamaru.png" },
-  { id: "isagi", name: "Isagi Yoichi", avatar: "/img/characters/Isagi_Yoichi.png" },
+  { id: "isagi", name: "Isagi Yoichi", avatar: "/img/characters/Isagi_Yoichi.jpeg" },
   { id: "rin", name: "Itoshi Rin", avatar: "/img/characters/Itoshi_Rin.png" },
   { id: "bachira", name: "Meguru Bachira", avatar: "/img/characters/Meguru_Bachira.png" },
   { id: "nagi", name: "Seishiro Nagi", avatar: "/img/characters/Seishiro_Nagi.png" },
